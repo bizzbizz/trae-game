dae151ead1fb28c2b08800d7a7726ad50da8ee76
bizhan <bizz.pt@gmail.com>
2025-03-25 21:40:09 +0100

    GamePopupResources created

---
+++ frontend/src/components/GamePanelResources.ts
@@ -0,0 +1,26 @@
+import { GamePanel, GameButton, GamePopup, DEFAULT_BUTTON_SIZE } from '../types/ui';
+import { Resource } from '../types/economy';
+
+export class GamePopupResources implements GamePopup {
+    buttons: GameButton[] = [];
+    onClose: () => void;
+
+    constructor(onClose: () => void) {
+        this.onClose = onClose;
+    }
+}
+
+export class GamePanelResources implements GamePanel {
+    buttons: GameButton[] = [];
+
+    constructor(resources: Resource[]) {
+        this.buttons = resources.map(resource => ({
+            emoji: resource.Icon,
+            tooltip: resource.ResourceId,
+            size: DEFAULT_BUTTON_SIZE,
+            popup: new GamePopupResources(() => {
+                // Close popup implementation
+            })
+        }));
+    }
+}
